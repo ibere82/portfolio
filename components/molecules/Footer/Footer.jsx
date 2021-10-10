446879456c96a31758e450b2a59fff9ce66d2efc
Iberê <ibere82@gmail.com>
2021-10-10 20:53:26 -0300

    more fix rel

--- components/molecules/Footer/Footer.jsx
+++ components/molecules/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className={css.container}>
       <div >
-        <p>&copy; 2021  <a target='_blank' title="Iberê' s linkedin page" href='https://www.linkedin.com/in/iber%C3%AA-abondanza-kuhlmann-0691b32a/'>Iberê Abondanza Kuhlmann</a></p>
+        <p>&copy; 2021  <a target='_blank' rel="noreferrer"  title="Iberê' s linkedin page" href='https://www.linkedin.com/in/iber%C3%AA-abondanza-kuhlmann-0691b32a/'>Iberê Abondanza Kuhlmann</a></p>
       </div>
       <Menu />
     </div >
